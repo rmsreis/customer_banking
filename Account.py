924823d7ac987ebfa13f10546b51cd86e3c66c3d
rmsreis <robertomsreis@gmail.com>
2024-06-02 22:11:23 -0500

    changes in the Account and cd_account.py

--- Account.py
+++ Account.py
@@ -2,16 +2,32 @@
 
 class Account:
     """Creating an Account class with methods"""
-    def __init__(self, balance, interest):
+    def __init__(self, balance=0.0, interest_rate=0.0):
+        """
+        Initialize an Account with a balance and interest rate.
+        """
         self.balance = balance
-        self.interest = interest
+        self.interest_rate = interest_rate
 
     # This method sets the balance of the account.
     def set_balance(self, balance):
-        """Sets the balance for the for the account"""
+        """
+        Set the account balanc
+        """
         self.balance = balance
 
     # The method sets the interest gained for the account.
     def set_interest(self, interest):
-        """Sets the interest gained for the the account"""
+        """
+        Set the account interest rate 
+        """
         self.interest = interest
+    
+    def update_balance_with_interest(self):
+        """
+        Update the balance with the earned interest and 
+        return the updated balance and interest earned.
+        """
+        interest = self.calculate_interest()
+        self.balance += interest
+        return self.balance, interest
